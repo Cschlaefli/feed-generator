55c5cf2749f0abed83226569d09754efa1de421a
Cade Schlaefli <cade.schlaefli@gmail.com>
2025-09-05 14:44:53 -0500

    fix: fix tag insertion

--- src/subscription.ts
+++ src/subscription.ts
@@ -76,11 +76,8 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
         .values(justPosts)
         .onConflict((oc) => oc.doNothing())
         .execute();
-      //   .compile();
-      // console.log('Inserting posts', postInsert.sql, postInsert.parameters);
-      // await this.db.executeQuery(postInsert);
       for(const post of postsToCreate.values()) {
-        const newTags = post.tags.map((tag) => ({ value: tag.toLowerCase() }));
+        const newTags = post.tags.map((tag) => tag.toLowerCase());
         // if tags empty
         if (newTags.length <= 0){
           console.log("No tags for post", post.uri);
@@ -88,21 +85,35 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
         }else{
           console.log("Adding tags for post", post.tags.length);
         }
-        const tagInsert = await this.db.insertInto('tag')
-            .values(newTags)
-            .onConflict((oc) => oc.column('value').doNothing())
-            .returning("id")
-            .compile();
-        // console.log('Inserting tags', tagInsert.sql, tagInsert.parameters);
-        const createTags = await this.db.executeQuery(tagInsert);
-        // console.log("Created/fetched tags ", createTags.rows);
-        // console.log("Post tags ", post.tags);
+        const newTagIds : {id : number}[] = [];
+        const insertTags : {value: string}[] = [];
+        for(const tag of newTags) {
+          const existing = await this.db
+            .selectFrom('tag')
+            .selectAll()
+            .where('value', '=', tag)
+            .executeTakeFirst();
+          if(existing) {
+            newTagIds.splice(parseInt(tag), 1);
+          }else{
+            insertTags.push({value: tag});
+          }
+        }
 
-        const postTags = (createTags?.rows?.filter((tag) => tag?.id) as {id : number}[])
-        .map((tag) => ({
+        if(insertTags.length !== 0){
+          const tagInsert = await this.db.insertInto('tag')
+              .values(insertTags)
+              .onConflict((oc) => oc.column('value').doNothing())
+              .returning("id")
+              .compile();
+          const createTags = await this.db.executeQuery(tagInsert);
+          newTagIds.push(...(createTags.rows as {id : number}[]));
+        }
+
+        const postTags = newTagIds.map((id) => ({
           uri: post.uri,
-          tag_id: tag.id,
-        })) ?? []
+          tag_id: id.id,
+        }));
         if (postTags.length > 0) {
           await this.db
             .insertInto('post_tags')
